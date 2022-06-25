64b709849acfc8f0a28cbff2d7080e84b79f589d
Tae Woo Kim <kim3434@purdue.edu>
2022-06-24 20:56:48 -0500

    updated setup page

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import '@aws-amplify/ui-react/styles.css'
 
 import Amplify from 'aws-amplify'
-// import config from './aws-exports'
+import config from './aws-exports'
 import { AmplifyProvider } from '@aws-amplify/ui-react'
 
-// Amplify.configure(config)
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
