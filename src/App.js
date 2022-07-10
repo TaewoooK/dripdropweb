f8d02a32fb250fab63fecad9353ca51b6e6a43c8
Joel John <joelj10001@gmail.com>
2022-07-10 18:55:40 -0400

    Basic File Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import {
   withAuthenticator
 } from '@aws-amplify/ui-react'
@@ -8,32 +6,22 @@ function App({
   signOut,
   user
 }) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <p>
-            {" "}
-            {user.attributes.email}{" "}
-            <button onClick={signOut}> Sign out </button>{" "}
-          </p>
-        </div>{" "}
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Drip Drop </h1>{" "}
-        <p>
-          Edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
-    </div>
-  );
+  return null
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <div>
+  //         <p>
+  //           {" "}
+  //           {user.attributes.email}{" "}
+  //           <button onClick={signOut}> Sign out </button>{" "}
+  //         </p>
+  //       </div>{" "}
+       
+       
+  //     </header>{" "}
+  //   </div>
+  // );
 }
 
 export default withAuthenticator(App);
