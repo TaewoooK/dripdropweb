b1fb8bb3786a21102ed90946ca341bfaae21673a
Tae Woo Kim <kim3434@purdue.edu>
2022-08-07 19:34:45 -0500

    changed function App to class App

--- src/App.js
+++ src/App.js
@@ -3,37 +3,41 @@ import './App.css';
 import {
   withAuthenticator
 } from '@aws-amplify/ui-react'
+import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
 
-function App({
-  signOut,
-  user
-}) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
+async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <button onClick={signOut}>sign out</button>
+          </div>
+          <h1> Drip Drop </h1>{" "}
           <p>
-            {" "}
-            {user.attributes.email}{" "}
-            <button onClick={signOut}> Sign out </button>{" "}
-          </p>
-        </div>{" "}
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Drip Drop </h1>{" "}
-        <p>
-          Edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
-    </div>
-  );
+            Edit <code> src / App.js </code> and save to reload.{" "}
+          </p>{" "}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React{" "}
+          </a>{" "}
+        </header>{" "}
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App);
