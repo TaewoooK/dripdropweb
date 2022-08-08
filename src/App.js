adf65742ea3149cbdd77daf549d35d1dea8ec3a5
Tae Woo Kim <kim3434@purdue.edu>
2022-08-07 21:38:12 -0500

    recent changes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ import {
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
 
-async function signOut() {
-    try {
-        await Auth.signOut();
-    } catch (error) {
-        console.log('error signing out: ', error);
-    }
-}
+let appName = 'dripdrop'
 
-class App extends Component{
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <button onClick={signOut}>sign out</button>
-          </div>
-          <h3> dripdrop </h3>
-        </header>
-      </div>
-    );
-  }
+function App({
+  signOut,
+  user
+}) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>
+            {user.attributes.email}
+            <button onClick={signOut}> Sign out </button>
+          </p>
+        </div>
+        <h3> {appName} </h3>
+      </header>
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
